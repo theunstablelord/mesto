d126fdbbf2fed383a6d3298a58514dc6af4e39e5
Artem Zakharov <kennyohmygod@yandex.ru>
2023-05-20 20:05:57 +0300

    feat: функционал добавления карточки + лайк с удалением

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,28 @@
 const elementsList = document.querySelector('.elements__list');
 const elementTemplate = document.querySelector("#element-template").content;
 const elementItem = elementTemplate.querySelector('.element');
-
-const popup = document.querySelector('.popup');
+const btnPopupClose = document.querySelectorAll('.popup__close-btn');
 const btnProfileEdit = document.querySelector('.profile__edit-btn');
-const btnPopupClose = document.querySelector('.popup__close-btn');
-const formElement = document.querySelector('.popup__form')
-const nameInput = document.querySelector('.popup__input_user_name')
-const jobInput = document.querySelector('.popup__input_user_about')
-const profileName = document.querySelector('.profile__name')
-const profileAbout = document.querySelector('.profile__about')
+const btnElementAdd = document.querySelector('.profile__add-btn');
+const popupProfileEdit = document.querySelector('.popup__profile-edit');
+const popupElementAdd = document.querySelector('.popup__element-add');
+const formElementProfileEdit = document.querySelector('.popup__form_profile_edit');
+const formElementAdd = document.querySelector('.popup__form_element_add');
+const nameInputProfileEdit = document.querySelector('.popup__input_user_name');
+const nameInputElementAdd = document.querySelector('.popup__input_element_name');
+const jobInput = document.querySelector('.popup__input_user_about');
+const urlInput = document.querySelector('.popup__input_element_link');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
 
+//Функции открытия/закрытия попапа
+function openPopup(item) {
+    item.classList.add('popup_opened');
+}
+
+function closePopup(item) {
+    item.classList.remove('popup_opened');
+}
 
 function createElement (element) {
     const createItem = elementItem.cloneNode(true);
@@ -20,6 +32,13 @@ function createElement (element) {
     elementName.textContent = element.name;
     elementImage.src = element.link;
     elementImage.alt = element.name;
+
+    createItem.querySelector('.element__like-btn').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('element__like-btn_active');
+    });
+    createItem.querySelector('.element__delete-btn').addEventListener('click', function() {
+        createItem.remove();
+    });
     
     return createItem;
 };
@@ -33,29 +52,38 @@ function addInitialElement(itemAdd) {
     addInitialElement(itemAdd);
   });
   
-
-//Функции открытия попапа
-function openPopup () {
-    popup.classList.add('popup_opened');
-
-    nameInput.value = profileName.textContent; 
-    jobInput.value = profileAbout.textContent;
-}
-
-function closePopup () {
-    popup.classList.remove('popup_opened');
-}
-
-
-function handleFormSubmit (evt) {
+function handleFormProfileEditSubmit (evt) {
     evt.preventDefault(); 
     
-    profileName.textContent = `${nameInput.value}`;
-    profileAbout.textContent = `${jobInput.value}`;
+    profileName.textContent = nameInputProfileEdit.value;
+    profileAbout.textContent = jobInput.value;
+    closePopup(popupProfileEdit);
+};
 
-    closePopup();
-}
+function handleFormElementAddSubmit (evt) {
+    evt.preventDefault(); 
+    
+    elementsList.prepend(createElement({ name: nameInputElementAdd.value, link: urlInput.value }));
+    formElementAdd.reset();
+    closePopup(popupElementAdd);
+};
 
-btnProfileEdit.addEventListener('click', openPopup);
-btnPopupClose.addEventListener('click', closePopup);
-popup.addEventListener('submit', handleFormSubmit); 
+btnProfileEdit.addEventListener('click', function () {
+    openPopup(popupProfileEdit);
+    nameInputProfileEdit.value = profileName.textContent;
+    jobInput.value = profileAbout.textContent;
+});
+
+btnElementAdd.addEventListener('click', function () {
+    openPopup(popupElementAdd);
+});
+
+btnPopupClose.forEach(element => {
+    element.addEventListener('click', function (evt) {
+     const popup = evt.target.closest('.popup');
+     closePopup(popup);
+    });
+});
+
+formElementProfileEdit.addEventListener('submit', handleFormProfileEditSubmit);
+formElementAdd.addEventListener('submit', handleFormElementAddSubmit);
